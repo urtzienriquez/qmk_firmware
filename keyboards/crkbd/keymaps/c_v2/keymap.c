161a1264b64f09a56cfcc1d01bb11f7c1baf053f
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2025-09-14 20:04:50 +0200

    update

--- keyboards/crkbd/keymaps/c_v2/keymap.c
+++ keyboards/crkbd/keymaps/c_v2/keymap.c
@@ -117,37 +117,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         _______, KC_LSFT, KC_LCTL, KC_LGUI, KC_LALT, XXXXXXX,                XXXXXXX, KC_LALT, KC_LGUI, KC_LCTL, KC_LSFT, _______,
         _______, XXXXXXX, LCS(KC_X), LCS(KC_C), LCS(KC_V), XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                                               _______, TO(0), KC_ENT,   KC_SPC, XXXXXXX, XXXXXXX
+                                               XXXXXXX, TO(0), KC_ENT,   KC_SPC, XXXXXXX, XXXXXXX
     ),
     [2] = LAYOUT_split_3x6_3(
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                                               _______, TO(0), KC_ENT,   KC_SPC, XXXXXXX, XXXXXXX
+                                               XXXXXXX, TO(0), KC_ENT,   KC_SPC, XXXXXXX, XXXXXXX
     ),
     [3] = LAYOUT_split_3x6_3(
         XXXXXXX, XXXXXXX, XXXXXXX, MS_WHLU, XXXXXXX, MS_BTN2,                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, MS_ACL0, MS_WHLL, MS_WHLD, MS_WHLR, MS_BTN1,                MS_LEFT, MS_DOWN, MS_UP,   MS_RGHT, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, MS_BTN3,                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                                               _______, TO(0), KC_ENT,   KC_SPC, XXXXXXX, XXXXXXX
+                                               XXXXXXX, TO(0), KC_ENT,   KC_SPC, XXXXXXX, XXXXXXX
     ),
     [4] = LAYOUT_split_3x6_3(
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                KC_LBRC, KC_7, KC_8, KC_9, KC_0, KC_RBRC,
         _______, XXXXXXX, XXXXXXX, XXXXXXX, OSM(MOD_LSFT), XXXXXXX,          KC_MINS, KC_4, KC_5, KC_6, KC_DOT, _______,
         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                KC_EQUAL,  KC_1, KC_2, KC_3, KC_SLSH, KC_ASTR,
-                                               _______, TO(0), KC_ENT,   KC_SPC, TO(5), XXXXXXX
+                                               XXXXXXX, TO(0), KC_ENT,   KC_SPC, TO(5), XXXXXXX
     ),
     [5] = LAYOUT_split_3x6_3(
         KC_TAB, INV_EXCL, DEGREE, KC_PIPE, KC_AMPR, KC_EXLM,                 KC_EQUAL, KC_LCBR, KC_RCBR, KC_PERC, KC_ASTR, KC_BSPC,
         _______, INV_QUES, KC_AT, KC_TILD, KC_SLSH, KC_QUES,                 KC_UNDS, KC_LPRN, KC_RPRN, KC_DLR, KC_COLN, _______,
         _______, KC_CIRC, KC_QUOT, KC_LT, KC_GT, KC_MINS,                    KC_PLUS,  KC_LBRC, KC_RBRC, KC_HASH, KC_BSLS, KC_GRV,
-                                               _______, TO(0), KC_ENT,   KC_SPC, TO(4), XXXXXXX
+                                               XXXXXXX, TO(0), KC_ENT,   KC_SPC, TO(4), XXXXXXX
     ),
     [6] = LAYOUT_split_3x6_3(
         QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,
         RM_TOGG, RM_HUEU, RM_SATU, RM_VALU, XXXXXXX, XXXXXXX,                KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
         RM_NEXT, RM_HUED, RM_SATD, RM_VALD, XXXXXXX, XXXXXXX,                KC_PSCR, KC_SCRL, KC_PAUS, KC_INS, KC_DEL, XXXXXXX,
-                                               _______, TO(0), KC_ENT,   KC_SPC, TO(5), XXXXXXX
+                                               XXXXXXX, TO(0), KC_ENT,   KC_SPC, TO(5), XXXXXXX
     )
 };
 
