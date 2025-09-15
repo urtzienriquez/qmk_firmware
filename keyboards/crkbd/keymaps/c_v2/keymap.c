8f6eed8c0da0cd61cbe0388ff3fb346c3127a749
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2025-09-15 15:26:31 +0200

    add modifiers layer key in right side

--- keyboards/crkbd/keymaps/c_v2/keymap.c
+++ keyboards/crkbd/keymaps/c_v2/keymap.c
@@ -43,6 +43,7 @@ const uint16_t PROGMEM arrows_combo[] = {OSL(_NUMBERS), OSL(_SYMBOLS), COMBO_END
 const uint16_t PROGMEM mouse_combo[] = {KC_ENT, KC_SPC, COMBO_END};
 const uint16_t PROGMEM locknum_combo[] = {OSL(_NUMBERS), KC_SPC, COMBO_END};
 const uint16_t PROGMEM locksym_combo[] = {OSL(_SYMBOLS), KC_ENT, COMBO_END};
+const uint16_t PROGMEM function_combo[] = {KC_TAB, KC_BSPC, COMBO_END};
 const uint16_t PROGMEM ntilde_combo[] = {KC_N, KC_F, COMBO_END};
 const uint16_t PROGMEM acute_a_combo[] = {KC_A, KC_J, COMBO_END};
 const uint16_t PROGMEM acute_e_combo[] = {KC_E, KC_J, COMBO_END};
@@ -58,6 +59,7 @@ combo_t key_combos[] = {
     COMBO(mouse_combo, MO(_MOUSE)),
     COMBO(locknum_combo, TO(_NUMBERS)),
     COMBO(locksym_combo, TO(_SYMBOLS)),
+    COMBO(function_combo, OSL(_FUNCTION)),
     COMBO(ntilde_combo, NTILDE),
     COMBO(acute_a_combo, A_ACUTE),
     COMBO(acute_e_combo, E_ACUTE),
@@ -121,19 +123,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                                KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
         OSM(MOD_LALT), KC_A, KC_S, KC_D, KC_F, KC_G,                         KC_H, KC_J, KC_K, KC_L, KC_SCLN, OSM(MOD_LCTL),
         OSM(MOD_LSFT), KC_Z, KC_X, KC_C, KC_V, KC_B,                         KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_QUOT,
-                              MO(_MODIFIERS), OSL(_NUMBERS), KC_ENT,   KC_SPC, OSL(_SYMBOLS), OSL(_FUNCTION)
+                              MO(_MODIFIERS), OSL(_NUMBERS), KC_ENT,   KC_SPC, OSL(_SYMBOLS), MO(_MODIFIERS)
     ),
     [_NUMBERS] = LAYOUT_split_3x6_3(
         XXXXXXX, XXXXXXX, XXXXXXX, KC_LCBR, KC_RCBR, KC_EXLM,                KC_EQUAL, KC_7, KC_8, KC_9, KC_0, XXXXXXX,
         _______, XXXXXXX, XXXXXXX, KC_LPRN, KC_RPRN, OSM(MOD_LSFT),          KC_MINS, KC_4, KC_5, KC_6, KC_DOT, KC_COMM,
         _______, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, XXXXXXX,                KC_PLUS,  KC_1, KC_2, KC_3, KC_SLSH, KC_ASTR,
-                                         _______, TO(_BASE), KC_ENT,   KC_SPC, TO(_SYMBOLS), XXXXXXX
+                                         _______, TO(_BASE), KC_ENT,   KC_SPC, TO(_SYMBOLS), _______
     ),
     [_SYMBOLS] = LAYOUT_split_3x6_3(
         KC_TAB, INV_EXCL, DEGREE, KC_PIPE, KC_AMPR, KC_EXLM,                 KC_EQUAL, KC_LCBR, KC_RCBR, KC_DQUO, KC_ASTR, KC_BSPC,
         _______, INV_QUES, KC_AT, KC_TILD, KC_SLSH, KC_QUES,                 KC_UNDS, KC_LPRN, KC_RPRN, KC_DLR, KC_COLN, _______,
         _______, KC_CIRC, KC_LT, KC_GT, KC_HASH, KC_MINS,                    KC_PLUS, KC_LBRC, KC_RBRC, KC_PERC, KC_BSLS, KC_GRV,
-                                         _______, TO(_BASE), KC_ENT,   KC_SPC, TO(_NUMBERS), XXXXXXX
+                                         _______, TO(_BASE), KC_ENT,   KC_SPC, TO(_NUMBERS), _______
     ),
     [_MODIFIERS] = LAYOUT_split_3x6_3(
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
