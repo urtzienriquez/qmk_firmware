30adc7c6810763d31579e857d1c18d6c69a7757c
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2025-12-03 18:58:14 +0100

    update

--- keyboards/crkbd/keymaps/c_v2/keymap.c
+++ keyboards/crkbd/keymaps/c_v2/keymap.c
@@ -151,8 +151,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_NUMBERS] = LAYOUT_split_3x6_3(
         XXXXXXX, XXXXXXX, XXXXXXX, KC_LCBR, KC_RCBR, KC_EXLM,                KC_EQUAL, KC_7, KC_8, KC_9, KC_0, _______,
-        XXXXXXX, XXXXXXX, XXXXXXX, KC_LPRN, KC_RPRN, XXXXXXX,                KC_MINS, KC_4, KC_5, KC_6, KC_DOT, KC_ASTR,
-        XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, XXXXXXX,                KC_PLUS,  KC_1, KC_2, KC_3, KC_SLSH, KC_COMM,
+        XXXXXXX, XXXXXXX, XXXXXXX, KC_LPRN, KC_RPRN, XXXXXXX,                KC_MINS, KC_4, KC_5, KC_6, KC_DOT, KC_COMM,
+        XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, XXXXXXX,                KC_PLUS,  KC_1, KC_2, KC_3, KC_SLSH, KC_ASTR,
                                          _______, TO(_BASE), KC_ENT,    MT(MOD_LSFT, KC_SPC), TO(_SYMBOLS), _______
     ),
     [_SYMBOLS] = LAYOUT_split_3x6_3(
