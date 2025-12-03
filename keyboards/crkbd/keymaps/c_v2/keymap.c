ed28d2589eff972a43abbb141aa1d7cb765b26a9
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2025-12-03 18:53:16 +0100

    update

--- keyboards/crkbd/keymaps/c_v2/keymap.c
+++ keyboards/crkbd/keymaps/c_v2/keymap.c
@@ -145,20 +145,20 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_split_3x6_3(
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                                KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
-        KC_LALT, KC_A, KC_S, KC_D, KC_F, KC_G,                         KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_LCTL,
-        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,                         KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_QUOT,
+        KC_LALT, KC_A, KC_S, KC_D, KC_F, KC_G,                         KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,                         KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_LCTL,
                               MO(_MODIFIERS), OSL(_NUMBERS), KC_ENT,    MT(MOD_LSFT, KC_SPC), OSL(_SYMBOLS), MO(_MODIFIERS)
     ),
     [_NUMBERS] = LAYOUT_split_3x6_3(
         XXXXXXX, XXXXXXX, XXXXXXX, KC_LCBR, KC_RCBR, KC_EXLM,                KC_EQUAL, KC_7, KC_8, KC_9, KC_0, _______,
-        XXXXXXX, XXXXXXX, XXXXXXX, KC_LPRN, KC_RPRN, XXXXXXX,                KC_MINS, KC_4, KC_5, KC_6, KC_DOT, KC_COMM,
-        XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, XXXXXXX,                KC_PLUS,  KC_1, KC_2, KC_3, KC_SLSH, KC_ASTR,
+        XXXXXXX, XXXXXXX, XXXXXXX, KC_LPRN, KC_RPRN, XXXXXXX,                KC_MINS, KC_4, KC_5, KC_6, KC_DOT, KC_ASTR,
+        XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, XXXXXXX,                KC_PLUS,  KC_1, KC_2, KC_3, KC_SLSH, KC_COMM,
                                          _______, TO(_BASE), KC_ENT,    MT(MOD_LSFT, KC_SPC), TO(_SYMBOLS), _______
     ),
     [_SYMBOLS] = LAYOUT_split_3x6_3(
         KC_TAB, INV_EXCL, DEGREE, KC_PIPE, KC_AMPR, KC_EXLM,                 KC_EQUAL, KC_LCBR, KC_RCBR, KC_DQUO, KC_ASTR, _______,
-        XXXXXXX, INV_QUES, KC_AT, KC_TILD, KC_SLSH, KC_QUES,                 KC_UNDS, KC_LPRN, KC_RPRN, KC_DLR, KC_COLN, _______,
-        XXXXXXX, KC_CIRC, KC_LT, KC_MINS, KC_GT, KC_HASH,                    KC_PLUS, KC_LBRC, KC_RBRC, KC_PERC, KC_BSLS, KC_GRV,
+        XXXXXXX, INV_QUES, KC_AT, KC_TILD, KC_SLSH, KC_QUES,                 KC_UNDS, KC_LPRN, KC_RPRN, KC_DLR, KC_COLN, KC_GRV,
+        XXXXXXX, KC_CIRC, KC_LT, KC_MINS, KC_GT, KC_HASH,                    KC_PLUS, KC_LBRC, KC_RBRC, KC_PERC, KC_BSLS, _______,
                                          _______, TO(_BASE), KC_ENT,    MT(MOD_LSFT, KC_SPC), TO(_NUMBERS), _______
     ),
     [_MODIFIERS] = LAYOUT_split_3x6_3(
